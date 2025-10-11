a36917c471aac7ae5f479f60a89724e894ac210f
dmolinsky <diana.molinsky@iths.se>
2025-10-11 19:18:11 +0200

    ADD component RecipeGrid

---
+++ src/components/RecipeGrid/RecipeGrid.jsx
@@ -0,0 +1,15 @@
+import RecipeCard from "../RecipeCard/RecipeCard";
+
+function RecipeGrid() {
+	return (
+		<div className="recipe-grid">
+			<RecipeCard />
+			<RecipeCard />
+			<RecipeCard />
+			<RecipeCard />
+			<RecipeCard />
+		</div>
+	);
+}
+
+export default RecipeGrid;
