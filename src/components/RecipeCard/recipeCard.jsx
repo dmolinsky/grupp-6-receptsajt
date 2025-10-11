af934cf82c877210497239cda3a52421bd0937cf
dmolinsky <diana.molinsky@iths.se>
2025-10-11 19:17:17 +0200

    EDIT added export in RecipeCard

---
+++ src/components/RecipeCard/recipeCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function RecipeCard() {
+    return (
+        <div className="recipe-card">
+            <Link to="/recipe/pepparkakor">
+                <img
+                className="recipe-card-img"
+                src="https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/mpyhqrwdrvmxe4kdeklo.jpg"
+                alt="Pepparkakor"
+                />
+            </Link>
+            <h3 className="recipe-card-title">Pepparkakor</h3>
+            <div className="recipe-card-rating">★★★★☆</div>
+            <p className="recipe-card-description">
+                En klassiker under julen och detta receptet ger fantastiskt goda kakor
+                med smak av kanel, ...
+            </p>
+            <div className="recipe-card-details">
+                <span>XX Ingredienser</span>
+                <span>XX Minuter</span>
+            </div>
+        </div>
+    );
+}
+
+export default RecipeCard;
