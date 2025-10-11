72973dd3f9e8fe3a3b3d30344a770b5dd3150cdc
dmolinsky <diana.molinsky@iths.se>
2025-10-11 21:04:21 +0200

    EDIT formatted by Prettier

--- src/components/RecipeCard/recipeCard.jsx
+++ src/components/RecipeCard/recipeCard.jsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function RecipeCard() {
     return (
         <div className="recipe-card round-md">
             <Link to="/recipe/pepparkakor">
                 <img
-                className="recipe-card-img"
-                src="https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/mpyhqrwdrvmxe4kdeklo.jpg"
-                alt="Pepparkakor"
+                    className="recipe-card-img"
+                    src="https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/mpyhqrwdrvmxe4kdeklo.jpg"
+                    alt="Pepparkakor"
                 />
             </Link>
             <h3 className="recipe-card-title">Pepparkakor</h3>
-            <div className="recipe-card-rating"><p>★★★★☆</p></div>
+            <div className="recipe-card-rating">
+                <p>★★★★☆</p>
+            </div>
             <p className="recipe-card-description">
-                En klassiker under julen och detta receptet ger fantastiskt goda kakor
-                med smak av kanel, ...
+                En klassiker under julen och detta receptet ger fantastiskt goda
+                kakor med smak av kanel, ...
             </p>
             <div className="recipe-card-details">
                 <span>XX Ingredienser</span>
