7b2065d222da920a4e3967604b380807046aca53
dmolinsky <diana.molinsky@iths.se>
2025-10-11 19:40:34 +0200

    gDT added component RecipeGrid to page Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import SearchBar from '../../components/searchbar/SearchBar.jsx';
+import RecipeGrid from "../../components/RecipeGrid/RecipeGrid";
 
 function Home() {
     return (
-        <main>
-            <SearchBar />
+        <main className="home">
+             <SearchBar />
             <p>Home Page</p>
+            <RecipeGrid />
         </main>
     );
 }
