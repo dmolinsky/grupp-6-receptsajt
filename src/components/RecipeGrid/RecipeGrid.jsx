1af04f56993d63b3cd3b6c3524ef31c898fa9017
dmolinsky <diana.molinsky@iths.se>
2025-10-13 14:06:09 +0200

    EDIT implement functionality to fetch recipe data from API, map it to objects and put it in respective RecipeCard

--- src/components/RecipeGrid/RecipeGrid.jsx
+++ src/components/RecipeGrid/RecipeGrid.jsx
@@ -1,15 +1,40 @@
 import RecipeCard from '../RecipeCard/RecipeCard';
+import React, { useEffect, useState } from "react";
+import { getAllRecipes } from "../../utils/getAllRecipes";
+import { mapApiRecipes } from "../../utils/recipeMappers";
+
 
 function RecipeGrid() {
-    return (
-        <div className="recipe-grid">
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-        </div>
-    );
+	const [recipes, setRecipes] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		async function fetchRecipes() {
+			try {
+				const data = await getAllRecipes();
+				const mapped = mapApiRecipes(data);
+				setRecipes(mapped);
+			} catch (err) {
+				setError("Kunde inte hämta recept 😞");
+				console.error(err);
+			} finally {
+				setLoading(false);
+			}
+		}
+		fetchRecipes();
+	}, []);
+
+	if (loading) return <p>Laddar recept, vänta lite!</p>;
+	if (error) return <p>{error}</p>;
+
+	return (
+		<div className="recipe-grid">
+			{recipes.map((recipe) => (
+				<RecipeCard key={recipe.id} recipe={recipe} />
+			))}
+		</div>
+	);
 }
 
 export default RecipeGrid;
