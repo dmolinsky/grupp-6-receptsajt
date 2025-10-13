aadb04532768bdad1a29b4cb2467301b86316e75
dmolinsky <diana.molinsky@iths.se>
2025-10-13 13:27:45 +0200

    EDIT changed CSS import file and added class .text-center to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
-import './Header.css';
+import '../../Index.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <header className="header">
+        <header className="header text-center">
             <h1>Julreceptsajten</h1>
             <nav className="nav">
                 <Link to="/" className="button">
