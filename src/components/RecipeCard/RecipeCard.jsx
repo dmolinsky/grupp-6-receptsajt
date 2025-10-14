34b0a2b8006cdada1a56486c30c37788acffe80b
dmolinsky <diana.molinsky@iths.se>
2025-10-14 10:38:05 +0200

    EDIT swapped hard coded recipe data to data from API in RecipeCard

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,26 +1,27 @@
 import { Link } from 'react-router-dom';
 
-function RecipeCard() {
+function RecipeCard({ recipe }) {
     return (
         <div className="recipe-card round-md">
-            <Link to="/recipe/pepparkakor">
+            <Link to={`/recipe/${recipe.id}`}>
                 <img
                     className="recipe-card-img"
                     src="https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/mpyhqrwdrvmxe4kdeklo.jpg"
-                    alt="Pepparkakor"
+                    alt={recipe.title}
                 />
             </Link>
-            <h3 className="recipe-card-title">Pepparkakor</h3>
+            <h3 className="recipe-card-title">{recipe.title}</h3>
             <div className="recipe-card-rating">
                 <p>★★★★☆</p>
             </div>
             <p className="recipe-card-description">
-                En klassiker under julen och detta receptet ger fantastiskt goda
-                kakor med smak av kanel, ...
+                	{recipe.description.length > 100
+		            ? recipe.description.substring(0, 100) + "..."
+		            : recipe.description}
             </p>
             <div className="recipe-card-details">
-                <span>XX Ingredienser</span>
-                <span>XX Minuter</span>
+				<span>{recipe.ingredientsCount} ingredienser</span>
+				<span>{recipe.cookingTime} minuter</span>
             </div>
         </div>
     );
