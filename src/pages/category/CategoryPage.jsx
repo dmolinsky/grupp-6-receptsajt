9c552caaf9f0d71c165ec84d781a142185d199f2
dmolinsky <diana.molinsky@iths.se>
2025-10-15 09:54:18 +0200

    EDIT implemented RecipeGrid in page Category to show recipes based on category in URL

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,7 +1,13 @@
+import RecipeGrid from '../../components/RecipeGrid/RecipeGrid';
+import { useParams } from "react-router-dom";
+
 function CategoryPage() {
+    const { categoryId } = useParams();
+    
     return (
         <main>
-            <p>Category Page</p>
+            <p>{categoryId}</p>
+            <RecipeGrid category={categoryId}/>
         </main>
     );
 }
