99b2d99f73311b3c024b2755b2e68b68ca5f24dc
dmolinsky <diana.molinsky@iths.se>
2025-10-15 09:44:37 +0200

    EDIT updated RecipeGrid to be able to show recipes based on category and search query as well

--- src/components/RecipeGrid/RecipeGrid.jsx
+++ src/components/RecipeGrid/RecipeGrid.jsx
@@ -1,18 +1,30 @@
 import RecipeCard from '../RecipeCard/RecipeCard';
 import React, { useEffect, useState } from "react";
 import { getAllRecipes } from "../../utils/getAllRecipes";
+import { getRecipesByCategory } from "../../utils/getRecipesByCategory";
 import { mapApiRecipes } from "../../utils/recipeMappers";
 
 
-function RecipeGrid() {
+function RecipeGrid({ category = null, searchQuery = null }) {
 	const [recipes, setRecipes] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 
+	
 	useEffect(() => {
 		async function fetchRecipes() {
 			try {
-				const data = await getAllRecipes();
+				let data;
+
+				if (category) {
+					data = await getRecipesByCategory(category);
+				} else if (searchQuery) {
+					// OBS PLACEHOLDER!!
+					data = await getAllRecipes(); // ska bytas till getRecipesBySearch(searchQuery) eller liknande
+				} else {
+					data = await getAllRecipes();
+				}
+
 				const mapped = mapApiRecipes(data);
 				setRecipes(mapped);
 			} catch (err) {
@@ -23,8 +35,7 @@ function RecipeGrid() {
 			}
 		}
 		fetchRecipes();
-	}, []);
-
+	}, [category, searchQuery]);
 	if (loading) return <p>Laddar recept, vänta lite!</p>;
 	if (error) return <p>{error}</p>;
 
