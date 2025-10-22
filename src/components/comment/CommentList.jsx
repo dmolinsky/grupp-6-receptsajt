1ba719eef986e4fe7784b905aa9b00dbf003bafe
Erik Spahic <erik.spahic@iths.se>
2025-10-22 17:10:54 +0200

    Add comment-list structure. Fix comment-list formatting.

--- src/components/comment/CommentList.jsx
+++ src/components/comment/CommentList.jsx
@@ -1,2 +1,22 @@
 import React from 'react';
 import CommentItem from './CommentItem';
+
+function CommentList({ comments }) {
+    if (!comments || comments.length === 0) {
+        return (
+            <p className="comment-empty">
+                Inga kommentarer än. Bli den första!
+            </p>
+        );
+    }
+
+    return (
+        <ul className="comment-list" role="list">
+            {comments.map((c) => (
+                <CommentItem key={c._id} comment={c} />
+            ))}
+        </ul>
+    );
+}
+
+export default CommentList;
