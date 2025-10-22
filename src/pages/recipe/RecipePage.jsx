e3cd9d6c1fa2f9d9ab4b08a423e690c99cab97a8
Erik Spahic <erik.spahic@iths.se>
2025-10-22 23:19:15 +0200

    Add comment-section to recipepage

--- src/pages/recipe/RecipePage.jsx
+++ src/pages/recipe/RecipePage.jsx
@@ -1,7 +1,10 @@
+import CommentSection from 'src/components/comment/CommentSection.jsx';
+
 function RecipePage() {
     return (
         <main>
             <p>Recipe page</p>
+            <CommentSection recipeId="demo" initialComments={[]} />
         </main>
     );
 }
