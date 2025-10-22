e333363f5ab1dd9d92ef61a2dfab78f601892091
dmolinsky <diana.molinsky@iths.se>
2025-10-22 11:08:24 +0200

    ADD component DifficultyBadge

---
+++ src/components/DifficultyBadge/DifficultyBadge.jsx
@@ -0,0 +1,32 @@
+function DifficultyBadge({ level }) {
+    let text = '';
+    let colorClass = '';
+
+    switch (level) {
+        case 1:
+            text = 'Lätt';
+            colorClass = 'difficulty-easy';
+            break;
+        case 2:
+            text = 'Medel';
+            colorClass = 'difficulty-medium';
+            break;
+        case 3:
+            text = 'Svår';
+            colorClass = 'difficulty-hard';
+            break;
+        default:
+            text = '';
+            colorClass = '';
+    }
+
+    return (
+        <div className="difficulty-badge-wrapper">
+            <div className={`difficulty-badge round-md ${colorClass}`}>
+                <span>{text}</span>
+            </div>
+        </div>
+    );
+}
+
+export default DifficultyBadge;
