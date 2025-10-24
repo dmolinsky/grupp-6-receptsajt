1856cb5ae7e41f9657996ea884fcd00f26cb9b4d
Slatebeard <max.ekberg@iths.se>
2025-10-24 13:01:12 +0200

    FEAT(get and post) add get and post functions for comments

---
+++ src/utils/getCommentsById.js
@@ -0,0 +1,18 @@
+const url = "https://grupp6-rbwot.reky.se/recipes/1/comments";
+
+export async function getCommentsById(id) {
+    try {
+        const response = await fetch(`https://grupp6-rbwot.reky.se/recipes/${id}/comments`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+        throw error;
+    }
+}
+
+
+export default getCommentsById;
---
+++ src/utils/postCommentById.js
@@ -0,0 +1,30 @@
+const url = "https://grupp6-rbwot.reky.se/recipes";
+
+// såhär ser scemat ut i API, får testa olika för att få in tid
+// const exampleSchema = {
+//     "comment": "string",
+//     "name": "string"
+// };
+
+export async function postCommentById(commentData, recipeId) {
+    try {
+        const response = await fetch(`${url}/${recipeId}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify(commentData),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.text();
+            throw new Error(`HTTP ${response.status}: ${errorData}`);
+        }
+    } catch (error) {
+        console.error('Error posting comment:', error);
+        throw error;
+    }
+}
+
+export default postCommentById;
