68713e1dc84e5b8c7133d0f34b80e3cf1f92922e
Erik Spahic <erik.spahic@iths.se>
2025-10-29 19:48:26 +0100

    ADD searchquery usestate in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
 import SearchBar from '../../components/searchbar/SearchBar.jsx';
 import RecipeGrid from '../../components/RecipeGrid/RecipeGrid';
 import CategoryList from '../../components/categoryList/CategoryList';
+import { useState } from 'react';
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState('');
     return (
         <main className="home">
             <CategoryList />
-            <SearchBar />
-            <RecipeGrid />
+            <SearchBar onSearch={(value) => setSearchQuery(value)} />
+            <RecipeGrid searchQuery={searchQuery} />
         </main>
     );
 }
