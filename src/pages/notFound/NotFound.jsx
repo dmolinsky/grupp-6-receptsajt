ddc537e148aabe6dfb6e8a9956ab47fdc406fe05
Linda Gustafsson <adnil90@gmail.com>
2025-10-29 20:53:53 +0100

    FEAT: implement 404 NotFound page with copy, image and home

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
     return (
-        <main>
-            <p>Error Page</p>
+        <main className="not-found">
+            <h1>404 – Page not found</h1>
+            <img src="tomte_letar.png" />
+            <p>
+                The page you are looking for does not exist or has been moved.
+            </p>
+            <Link to="/" className="btn">
+                Back to homepage
+            </Link>
+            {/* Optional: fun image */}
+            {/* <img src="/images/tomte-forstoringsglas.png" alt="Santa's helper is looking for a page" /> */}
         </main>
     );
 }
