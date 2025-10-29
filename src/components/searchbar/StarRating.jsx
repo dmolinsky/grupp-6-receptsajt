1c652f329200ecf48e9e9c31e0bfff96c100ed21
Akash003-1 <Akash.0015.s@gmail.com>
2025-10-29 02:37:06 +0100

    Lade till StarRating-komponent med stjärnvisning

--- src/components/searchbar/StarRating.jsx
+++ src/components/searchbar/StarRating.jsx
@@ -1,13 +1,19 @@
-import StarRating from '../path/till/StarRating';
-
 function StarRating({ rating }) {
-    const stars = Array.from({ length: 5 }, (_, i) => (
-        <span key={i}>{i < rating ? '★' : '☆'}</span>
-    ));
-    return <div>{stars}</div>;
+  const stars = Array.from({ length: 5 }, (_, i) => (
+    <span
+      key={i}
+      style={{
+        color: i < rating ? '#FFD700' : '#ccc',
+        fontSize: '20px',
+        marginRight: '2px'
+      }}
+    >
+      {i < rating ? '★' : '☆'}
+    </span>
+  ));
+  return <div>{stars}</div>;
 }
 
-export default StarRating; 
 
 
 
