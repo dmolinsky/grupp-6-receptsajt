a1980a2b604aa3c6b43355820ce4961c2b99b5c4
dmolinsky <diana.molinsky@iths.se>
2025-10-30 18:21:36 +0100

    refactor: extract validation logic from CommentForm into separate validateComment() function

--- src/components/comment/CommentForm.jsx
+++ src/components/comment/CommentForm.jsx
@@ -1,11 +1,6 @@
 import { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-
-const INVALID_TAGS = /[<>]/;
-const INVALID_NAME = /[^a-zA-ZåäöÅÄÖ\s'-]/;
-const INVALID_TEXT = /[^a-zA-ZåäöÅÄÖ0-9\s.,!?'"()\n-]/;
-const INVALID_NEWLINES = /\n{5,}/;
-const INVALID_LINKS = /https?:\/\//i;
+import { validateComment } from '../../utils/validateComment';
 
 function CommentForm({ onSubmit }) {
     const [name, setName] = useState('');
@@ -22,68 +17,12 @@ function CommentForm({ onSubmit }) {
     const nameLen = trimmedName.length;
     const textLen = trimmedText.length;
 
-    const errors = useMemo(() => {
-        const e = {};
-        if (touched.name && (nameLen < MIN_NAME || nameLen > MAX_NAME)) {
-            e.name =
-                nameLen === 0
-                    ? 'Vänligen fyll i ditt namn!'
-                    : `Ditt namn måste vara mellan ${MIN_NAME}-${MAX_NAME} tecken långt!`;
-        }
-        if (touched.text && (textLen < MIN_COMMENT || textLen > MAX_COMMENT)) {
-            e.text =
-                textLen === 0
-                    ? 'Din kommentar är helt tom!'
-                    : `Mellan ${MIN_COMMENT}-${MAX_COMMENT} tecken.`;
-        }
+    const errors = useMemo(
+        () => validateComment({ name, text, touched }),
+        [name, text, touched]
+    );
 
-        if (touched.text && INVALID_TAGS.test(trimmedText)) {
-            e.text =
-                'Kommentaren får inte innehålla HTML-taggar eller andra otillåtna tecken.';
-        }
-
-        if (touched.name && INVALID_TAGS.test(trimmedName)) {
-            e.name =
-                'Namnet får inte innehålla HTML-taggar eller andra otillåtna tecken.';
-        }
-
-        if (touched.name && INVALID_NAME.test(trimmedName)) {
-            e.name =
-                'Namnet får bara innehålla bokstäver, mellanslag och bindestreck.';
-        }
-
-        if (touched.text && INVALID_TEXT.test(trimmedText)) {
-            e.text = 'Kommentaren innehåller otillåtna tecken.';
-        }
-
-        if (touched.text && INVALID_NEWLINES.test(trimmedText)) {
-            e.text = 'Kommentaren får inte innehålla för många radbrytningar.';
-        }
-
-        if (touched.text && INVALID_LINKS.test(trimmedText)) {
-            e.text = 'Kommentaren får inte innehålla länkar.';
-        }
-
-        return e;
-    }, [
-        nameLen,
-        textLen,
-        touched.name,
-        touched.text,
-        trimmedName,
-        trimmedText,
-    ]);
-
-    const isValid =
-        nameLen >= MIN_NAME &&
-        nameLen <= MAX_NAME &&
-        textLen >= MIN_COMMENT &&
-        textLen <= MAX_COMMENT &&
-        !INVALID_TAGS.test(trimmedText) &&
-        !INVALID_TAGS.test(trimmedName) &&
-        !INVALID_TEXT.test(trimmedText) &&
-        !INVALID_NEWLINES.test(trimmedText) &&
-        !INVALID_LINKS.test(trimmedText);
+    const isValid = Object.keys(errors).length === 0;
 
     function handleSubmit(e) {
         e.preventDefault();
