f3b7a7db8deb29bd8a1f7be3952a9405ad499d8f
Erik Spahic <erik.spahic@iths.se>
2025-10-30 01:02:08 +0100

    ADD functionality for header-link and clear-button to reset searchbar and grid on click.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../../index.css';
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleTitleClick = (e) => {
+        e.preventDefault();
+            navigate('/', { state: { clearSearch: true } });
+
+    };
+
     return (
         <header className="header text-center">
             <h1>
-                <Link to="/" className="title-link">
+                <Link to="/" onClick={handleTitleClick} className="title-link">
                     Julreceptsajten
                 </Link>
             </h1>
