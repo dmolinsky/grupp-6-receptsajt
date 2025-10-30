dff920091cba03b4706c0d7f3492f8fd11254c8c
Linda Gustafsson <adnil90@gmail.com>
2025-10-30 19:45:41 +0100

    FIX: update image alt in NotFoundPage

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -4,15 +4,16 @@ function NotFoundPage() {
     return (
         <main className="not-found">
             <h1>404 – Page not found</h1>
-            <img src="tomte_letar.png" />
+            <img
+                src="tomte_letar.png"
+                alt="Santa's helper is looking for a page"
+            />
             <p>
                 The page you are looking for does not exist or has been moved.
             </p>
             <Link to="/" className="btn">
                 Back to homepage
             </Link>
-            {/* Optional: fun image */}
-            {/* <img src="/images/tomte-forstoringsglas.png" alt="Santa's helper is looking for a page" /> */}
         </main>
     );
 }
