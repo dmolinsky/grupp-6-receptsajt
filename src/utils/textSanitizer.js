07f1bd3cf7840f98f06a105aab6fb34fd40ba189
Slatebeard <max.ekberg@iths.se>
2025-11-03 19:48:08 +0100

    FIX change to only look at script
    no more regex

--- src/utils/textSanitizer.js
+++ src/utils/textSanitizer.js
@@ -1,22 +1,5 @@
-function sanitizeText(text) {
-    return (
-        text
-            .replace(/\s*<script[\s\S]*?>[\s\S]*?<\/script>\s*/gi, '')
-            .replace(
-                /\s*<(iframe|svg|object|embed)[\s\S]*?>[\s\S]*?<\/\1>\s*/gi,
-                ''
-            )
-            .replace(/<\/?[^>]+(>|$)/g, '')
-            .replace(/&lt;/g, '<')
-            .replace(/&gt;/g, '>')
-            .replace(/&amp;/g, '&')
-            .replace(/&quot;/g, '"')
-            .replace(/&#39;/g, "'")
-
-            //fix whitespace
-            .replace(/\s+/g, ' ')
-            .trim()
-    );
+function textSanitizer(text) {
+    return text.replace(/<script[\s\S]*?>[\s\S]*?<\/script>/gi, '');
 }
 
-export default sanitizeText;
+export default textSanitizer;
