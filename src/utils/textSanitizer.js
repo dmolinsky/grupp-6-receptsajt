879f36bc6e5ea706060bd2f23a8e96c28e9a71bd
Slatebeard <max.ekberg@iths.se>
2025-11-03 16:13:57 +0100

    FIX format

--- src/utils/textSanitizer.js
+++ src/utils/textSanitizer.js
@@ -1,19 +1,22 @@
-
 function sanitizeText(text) {
-    return text
-        .replace(/\s*<script[\s\S]*?>[\s\S]*?<\/script>\s*/gi, "")
-        .replace(/\s*<(iframe|svg|object|embed)[\s\S]*?>[\s\S]*?<\/\1>\s*/gi, "")
-        .replace(/<\/?[^>]+(>|$)/g, "")
-        .replace(/&lt;/g, "<")
-        .replace(/&gt;/g, ">")
-        .replace(/&amp;/g, "&")
-        .replace(/&quot;/g, '"')
-        .replace(/&#39;/g, "'")
+    return (
+        text
+            .replace(/\s*<script[\s\S]*?>[\s\S]*?<\/script>\s*/gi, '')
+            .replace(
+                /\s*<(iframe|svg|object|embed)[\s\S]*?>[\s\S]*?<\/\1>\s*/gi,
+                ''
+            )
+            .replace(/<\/?[^>]+(>|$)/g, '')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&amp;/g, '&')
+            .replace(/&quot;/g, '"')
+            .replace(/&#39;/g, "'")
 
-
-        //fix whitespace
-        .replace(/\s+/g, " ")
-        .trim();
+            //fix whitespace
+            .replace(/\s+/g, ' ')
+            .trim()
+    );
 }
 
 export default sanitizeText;
